5e35cf6496d59a477bfdb137e3d0f0359add06d5
Bu11t0N <133641218+Bu11t0N@users.noreply.github.com>
2025-10-17 17:59:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 document.querySelectorAll('.course-card').forEach(card => {
     card.addEventListener('click', () => {
-        alert(`Вы выбрали ${card.querySelector('.course-title').innerText}`);
-        // Организация открытия соответствующего модуля курса
+        const title = card.querySelector('.course-title').innerText.trim();
+
+        // Проверяем, какая карточка нажата
+        if (title === "Фишинг") {
+            window.location.href = "phishing.html"; // переход на страницу фишинга
+        } else {
+            alert(Раздел "${title}" пока в разработке.);
+        }
     });
 });
